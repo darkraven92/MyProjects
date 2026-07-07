6947b95b1464be0ac11665bf8828886c7ae23dfa
darkraven92 <pasty-dose-tremble@duck.com>
2026-07-07 21:25:50 +0200

    new code csharp

--- CSharp/math/Program.cs
+++ CSharp/math/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Math
+{
+    class Triangle
+    {
+        public int Bas;
+        public int Height;
+
+        //Konstruktor
+        public Triangle(int bas, int height)
+        {
+            Bas = bas;
+            Height = height;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle(2, 3);
+            int area = (triangle.Bas * triangle.Height) / 2;
+            Console.WriteLine(area);
+        }
+    }
+}
