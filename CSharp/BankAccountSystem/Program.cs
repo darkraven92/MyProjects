59e4e5b1b0561b4f2c1f84b7f0f3e64c140aec43
darkraven92 <pasty-dose-tremble@duck.com>
2026-07-22 20:52:15 +0200

    commit

---
+++ CSharp/BankAccountSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿using Namespace;
+
+class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World!");
+	}
+
+}	
---
+++ CSharp/ProCSharp10With.Net6/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Calc{
+	public int Add(int x, int y){
+		return x + y;
+	}
+}
+
+class Program{
+	public static void Main(){
+		Calc c = new Calc();
+		int ans = c.Add(2,29);
+		Console.WriteLine(ans);
+
+	}
+}
+
