51cb6b2744363d4a521c6a45bde4b65aa1d8ebfe
darkraven92 <pasty-dose-tremble@duck.com>
2026-09-18 21:15:05 +0200

    Delete Projects/wow-bot-cachyos directory

--- Projects/wow-bot-cachyos/Memory/IProcessMemory.cs
+++
@@ -1,9 +0,0 @@
-namespace WowBot.CachyOS.Memory;
-
-public interface IProcessMemory : IDisposable
-{
-    int ProcessId { get; }
-    void ReadBytes(ulong address, Span<byte> destination);
-    T Read<T>(ulong address) where T : unmanaged;
-    uint ReadPointer32(ulong address);
-}
--- Projects/wow-bot-cachyos/Runtime/NpcInteractionTestRunner.cs
+++
@@ -1,3 +0,0 @@
-// Intentionally empty compatibility file.
-// Kept so that `unzip -o` replaces obsolete local versions of this source file.
-// The bounded NPC test is implemented by NpcTargetTestRunner.
